e88f37384e4709abfbf6f68b13f8e4f827b6bda8
colives <carlos.olives.camps@gmail.com>
2020-08-06 10:59:29 +0200

    __init__ creation

--- epsapy/__init__.py
+++ epsapy/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  1 18:34:10 2019
-
-@author: carlosolives
+epsapy
 Electrical Power Systems Analysis
 """
 
 import epsapy
+__autor__ = 'Carlos Olives'
+__vesion__ = '0.0.1'
+__revision__ = '0.0.1'
