92bd259a29ba3c16dbda8e978357b60f139a504e
colives <carlos.olives.camps@gmail.com>
2021-05-31 17:49:55 +0100

    phases parameter removed

--- examples/example1.py
+++ examples/example1.py
@@ -34,19 +34,19 @@ cigre_eu.bus('busR18', 230)
 cigre_eu.line('line1',' busR0', 'busR1', ['a','b','c','n'], 1, 1, 1, 1, 1, 1, 1)
 
 # Generators
-cigre_eu.gridformer('grid',  'busR0', ['a','b','c','n'], 20e3/np.sqrt(3), 0.00)
-cigre_eu.gridformer('bat1',  'busR6', ['a','b','c','n'], 234.60, 0.00)
-cigre_eu.gridformer('bat2', 'busR10', ['a','b','c','n'], 234.60, 0.00)
+cigre_eu.gridformer('grid',  'busR0', 20e3/np.sqrt(3), 0.00)
+cigre_eu.gridformer('bat1',  'busR6', 234.60, 0.00)
+cigre_eu.gridformer('bat2', 'busR10', 234.60, 0.00)
 
 # 15 wt1
 # 16 pv1
 # 18 pv2
 
 # Loads name, bus, phases, p_0, q_0, k_z, k_i, k_p)
-cigre_eu.load('load1', 'busR11', ['a','b','c','n'], [ 850., 1700., 2295.], [ 526.78, 1053.57, 1422.31], 0.4, 0.3, 0.3)
-cigre_eu.load('load2', 'busR15', ['a','b','c','n'], [4080., 5440., 6800.], [2528.56, 3371.41, 4214.26], 0.4, 0.3, 0.3)
-cigre_eu.load('load3', 'busR16', ['a','b','c','n'], [4080., 5440., 6800.], [2528.56, 3371.41, 4214.26], 0.4, 0.3, 0.3)
-cigre_eu.load('load4', 'busR17', ['a','b','c','n'], [   0.,    0., 2295.], [     0.,      0., 1422.31], 0.4, 0.3, 0.3)
-cigre_eu.load('load5', 'busR18', ['a','b','c','n'], [1360., 2720., 3400.], [ 842.85, 1685.70, 2107.13], 0.4, 0.3, 0.3)
+cigre_eu.load('load1', 'busR11', [ 850., 1700., 2295.], [ 526.78, 1053.57, 1422.31], 0.4, 0.3, 0.3)
+cigre_eu.load('load2', 'busR15', [4080., 5440., 6800.], [2528.56, 3371.41, 4214.26], 0.4, 0.3, 0.3)
+cigre_eu.load('load3', 'busR16', [4080., 5440., 6800.], [2528.56, 3371.41, 4214.26], 0.4, 0.3, 0.3)
+cigre_eu.load('load4', 'busR17', [   0.,    0., 2295.], [     0.,      0., 1422.31], 0.4, 0.3, 0.3)
+cigre_eu.load('load5', 'busR18', [1360., 2720., 3400.], [ 842.85, 1685.70, 2107.13], 0.4, 0.3, 0.3)
 
 # Run Power Flow
--- src/epsapy/elements.py
+++ src/epsapy/elements.py
@@ -44,29 +44,27 @@ class Bus(object):
 
 class Branch(object):
 
-    def __init__(self, name, bus_i, bus_j, phases):
+    def __init__(self, name, bus_i, bus_j):
         self.name = name
         self.bus_i = bus_i
         self.bus_j = bus_j
-        self.phases = phases
         self.id = int(str(bus_i)+str(bus_j))
 
 
 class Shunt(object):
 
-    def __init__(self, name, bus, phases):
+    def __init__(self, name, bus):
         self.name = name
         self.bus = bus
-        self.phases = phases
         
 
 class Line(Branch):
     
-    def __init__(self, name, bus_i, bus_j, phases, long, r_L, x_L, b_Li, g_Li, b_Lj, g_Lj):
-        super().__init__(name, bus_i, bus_j, phases)
+    def __init__(self, name, bus_i, bus_j, nc, long, r_L, x_L, b_Li, g_Li, b_Lj, g_Lj):
+        super().__init__(name, bus_i, bus_j)
         self.long = long
-        self.r_L = r_L*long*np.eye(len(phases))
-        self.x_L = x_L*long*np.eye(len(phases))
+        self.r_L = r_L*long*np.eye(nc)
+        self.x_L = x_L*long*np.eye(nc)
         self.b_Li = b_Li
         self.g_Li = g_Li
         self.b_Lj = b_Lj
@@ -79,8 +77,8 @@ class Line(Branch):
 
 class Transformer(Branch):
 
-    def __init__(self, name, bus_i, bus_j, r, x, phases, v_inom, v_jnom, conn):
-        super().__init__(name, bus_i, bus_j, phases)
+    def __init__(self, name, bus_i, bus_j, r, x, v_inom, v_jnom, conn):
+        super().__init__(name, bus_i, bus_j)
         self.v_inom = v_inom
         self.v_jnom = v_jnom
         self.conn = conn
@@ -89,8 +87,8 @@ class Transformer(Branch):
 class Load(Shunt):
     load_id = 0
     
-    def __init__(self, name, bus, phases, p_0, q_0, k_z, k_i, k_p):
-        super().__init__(name,bus,phases)
+    def __init__(self, name, bus, p_0, q_0, k_z, k_i, k_p):
+        super().__init__(name, bus)
         self.id = Load.load_id
         self.p_0 = p_0
         self.q_0 = q_0
@@ -103,8 +101,8 @@ class Load(Shunt):
 class GridFormer(Shunt):
     gf_id = 0
     
-    def __init__(self, name, bus, phases, v_ref, angle_a):
-        super().__init__(name, bus, phases)
+    def __init__(self, name, bus, v_ref, angle_a):
+        super().__init__(name, bus)
         self.v_ref = v_ref
         self.angle_a = angle_a
         self.id = GridFormer.gf_id
@@ -114,8 +112,8 @@ class GridFormer(Shunt):
 class Generator(Shunt):
     gen_id = 0
     
-    def __init__ (self,name,bus,phases,p_max,p_min,q_max,q_min):
-        super().__init__(name,bus,phases)
+    def __init__ (self, name, bus, p_max, p_min, q_max, q_min):
+        super().__init__(name, bus)
         self.p_max = p_max
         self.p_min = p_min
         self.q_max = q_max
@@ -155,39 +153,39 @@ class System(object):
         self.conns[bus] = list()
         self.n = bus+1
     
-    def line(self,name,bus_i,bus_j,phases,long,r_L,x_L,b_Li,g_Li,b_Lj,g_Lj):
+    def line(self, name, bus_i, bus_j, long, r_L, x_L, b_Li, g_Li, b_Lj, g_Lj):
         bus_i = self.info['Buses'][bus_i].id
         bus_j = self.info['Buses'][bus_j].id
         self.conns[bus_i] += [bus_j]
         self.conns[bus_j] += [bus_i]
-        System.new_element(self,'Lines',Line(name,bus_i,bus_j,phases,long,r_L,x_L,b_Li,g_Li,b_Lj,g_Lj))
+        System.new_element(self,'Lines',Line(name, bus_i, bus_j, self.num_cond, long, r_L, x_L, b_Li, g_Li, b_Lj, g_Lj))
     
-    def transformer(self,name,bus_i,bus_j,r,x,s_max,phases,v_inom,v_jnom,conn):
+    def transformer(self, name, bus_i, bus_j, r, x, s_max, v_inom, v_jnom, conn):
         bus_i = self.info['Buses'][bus_i].id
         bus_j = self.info['Buses'][bus_j].id
-        System.new_element(self,'Transformers',Transformer(name,bus_i,bus_j,r,x,s_max,phases,v_inom,v_jnom,conn))
+        System.new_element(self,'Transformers',Transformer(name, bus_i, bus_j, r, x, s_max, v_inom, v_jnom, conn))
     
-    def gridformer(self, name, bus, phases, v_ref, angle_a):
+    def gridformer(self, name, bus, v_ref, angle_a):
         bus = self.info['Buses'][bus].id
         v_b = self.elmts['Buses'][bus].v_b
-        System.new_element(self, 'Grid Formers', GridFormer(name, bus, phases, v_ref/v_b, angle_a))
+        System.new_element(self, 'Grid Formers', GridFormer(name, bus, v_ref/v_b, angle_a))
         self.elmts['Buses'][bus].type = 'gform'
         self.elmts['Buses'][bus].v = self.set_v(v_ref/v_b, angle_a)
     
-    def generator(self,name,bus,p_max,p_min,q_max,q_min):
+    def generator(self, name, bus, p_max, p_min, q_max, q_min):
         bus = self.info['Buses'][bus].id
-        System.new_element(self, 'Generators', Generator(name,bus,p_max,p_min,q_max,q_min))
+        System.new_element(self, 'Generators', Generator(name, bus, p_max, p_min, q_max, q_min))
         self.elmts['Buses'][bus].type = 'gfoll'
     
-    def load(self, name, bus, phases, p_0, q_0, k_z, k_i, k_p):
+    def load(self, name, bus, p_0, q_0, k_z, k_i, k_p):
         s_b = self.s_b
         bus = self.info['Buses'][bus].id
-        System.new_element(self, 'Loads', Load(name, bus, phases, np.array(p_0)/s_b, np.array(q_0)/s_b, k_z, k_i, k_p))
+        System.new_element(self, 'Loads', Load(name, bus, np.array(p_0)/s_b, np.array(q_0)/s_b, k_z, k_i, k_p))
         self.elmts['Buses'][bus].type = 'load'
     
-    def shunt(self,name,bus,phases):
+    def shunt(self,name,bus):
         bus = self.info['Buses'][bus].id
-        System.new_element(self, 'Shunts', Shunt(name,bus,phases))
+        System.new_element(self, 'Shunts', Shunt(name,bus))
     
     def set_v(self, v_ref, angle_a):
         n = self.num_phs
@@ -201,4 +199,4 @@ class System(object):
     
     def set_s(self, p, q):
         n = self.num_phs
-        return np.vectorize(complex)(n*[0],n*[0])
+        return np.vectorize(complex)(n*[0], n*[0])
