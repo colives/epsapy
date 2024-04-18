789e13e2acd32253c75b7e11b00397d482b5e0e6
colives <carlos.olives.camps@gmail.com>
2024-04-18 18:37:17 +0200

    routines rename

--- src/epsapy/routines/__init__.py
+++ src/epsapy/routines/__init__.py
@@ -5,3 +5,8 @@ epsapy
 Electrical Power Systems Analysis
 Package for epsapy analysis routines.
 """
+
+from .routines import eigen
+from .routines import pf
+from .routines import pf_fact
+from .routines import tds
